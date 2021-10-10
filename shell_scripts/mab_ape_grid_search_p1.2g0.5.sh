c5918a185531935c1e6d1b6c1f1ebceac314de0d
kyungjaelee <dlxhrl@snu.ac.kr>
2021-10-11 00:11:32 +0900

    increase sample size for grid search

--- shell_scripts/mab_ape_grid_search_p1.2g0.5.sh
+++ shell_scripts/mab_ape_grid_search_p1.2g0.5.sh
@@ -1,77 +1,77 @@
 for i in {1..9}
 do
-    python3 -m multi_armed_bandit.run_grid_search --noise pareto --moment 1.2 --scale 1.0 --mean one_hot --gap 0.5 --samples 5000 --arms 10 --algo ucb-truncated-mean --seed $i &
+    python3 -m multi_armed_bandit.run_grid_search --noise pareto --moment 1.2 --scale 1.0 --mean one_hot --gap 0.5 --samples 20000 --arms 10 --algo ucb-truncated-mean --seed $i &
 done
-python3 -m multi_armed_bandit.run_grid_search --noise pareto --moment 1.2 --scale 1.0 --mean one_hot --gap 0.5 --samples 5000 --arms 10 --algo ucb-truncated-mean --seed 10
+python3 -m multi_armed_bandit.run_grid_search --noise pareto --moment 1.2 --scale 1.0 --mean one_hot --gap 0.5 --samples 20000 --arms 10 --algo ucb-truncated-mean --seed 10
 
 for i in {1..9}
 do
-    python3 -m multi_armed_bandit.run_grid_search --noise pareto --moment 1.2 --scale 1.0 --mean one_hot --gap 0.5 --samples 5000 --arms 10 --algo ucb-catoni-mean --seed $i &
+    python3 -m multi_armed_bandit.run_grid_search --noise pareto --moment 1.2 --scale 1.0 --mean one_hot --gap 0.5 --samples 20000 --arms 10 --algo ucb-catoni-mean --seed $i &
 done
-python3 -m multi_armed_bandit.run_grid_search --noise pareto --moment 1.2 --scale 1.0 --mean one_hot --gap 0.5 --samples 5000 --arms 10 --algo ucb-catoni-mean --seed 10
+python3 -m multi_armed_bandit.run_grid_search --noise pareto --moment 1.2 --scale 1.0 --mean one_hot --gap 0.5 --samples 20000 --arms 10 --algo ucb-catoni-mean --seed 10
 
 for i in {1..9}
 do
-    python3 -m multi_armed_bandit.run_grid_search --noise pareto --moment 1.2 --scale 1.0 --mean one_hot --gap 0.5 --samples 5000 --arms 10 --algo ucb-median-of-mean --seed $i &
+    python3 -m multi_armed_bandit.run_grid_search --noise pareto --moment 1.2 --scale 1.0 --mean one_hot --gap 0.5 --samples 20000 --arms 10 --algo ucb-median-of-mean --seed $i &
 done
-python3 -m multi_armed_bandit.run_grid_search --noise pareto --moment 1.2 --scale 1.0 --mean one_hot --gap 0.5 --samples 5000 --arms 10 --algo ucb-median-of-mean --seed 10
+python3 -m multi_armed_bandit.run_grid_search --noise pareto --moment 1.2 --scale 1.0 --mean one_hot --gap 0.5 --samples 20000 --arms 10 --algo ucb-median-of-mean --seed 10
 
 for i in {1..9}
 do
-    python3 -m multi_armed_bandit.run_grid_search --noise pareto --moment 1.2 --scale 1.0 --mean one_hot --gap 0.5 --samples 5000 --arms 10 --algo dsee --seed $i &
+    python3 -m multi_armed_bandit.run_grid_search --noise pareto --moment 1.2 --scale 1.0 --mean one_hot --gap 0.5 --samples 20000 --arms 10 --algo dsee --seed $i &
 done
-python3 -m multi_armed_bandit.run_grid_search --noise pareto --moment 1.2 --scale 1.0 --mean one_hot --gap 0.5 --samples 5000 --arms 10 --algo dsee --seed 10
+python3 -m multi_armed_bandit.run_grid_search --noise pareto --moment 1.2 --scale 1.0 --mean one_hot --gap 0.5 --samples 20000 --arms 10 --algo dsee --seed 10
 
 for i in {1..9}
 do
-    python3 -m multi_armed_bandit.run_grid_search --noise pareto --moment 1.2 --scale 1.0 --mean one_hot --gap 0.5 --samples 5000 --arms 10 --algo gsr --seed $i &
+    python3 -m multi_armed_bandit.run_grid_search --noise pareto --moment 1.2 --scale 1.0 --mean one_hot --gap 0.5 --samples 20000 --arms 10 --algo gsr --seed $i &
 done
-python3 -m multi_armed_bandit.run_grid_search --noise pareto --moment 1.2 --scale 1.0 --mean one_hot --gap 0.5 --samples 5000 --arms 10 --algo gsr --seed 10
+python3 -m multi_armed_bandit.run_grid_search --noise pareto --moment 1.2 --scale 1.0 --mean one_hot --gap 0.5 --samples 20000 --arms 10 --algo gsr --seed 10
 
 for i in {1..9}
 do
-    python3 -m multi_armed_bandit.run_grid_search --noise pareto --moment 1.2 --scale 1.0 --mean one_hot --gap 0.5 --samples 5000 --arms 10 --algo ape-gamma --seed $i &
+    python3 -m multi_armed_bandit.run_grid_search --noise pareto --moment 1.2 --scale 1.0 --mean one_hot --gap 0.5 --samples 20000 --arms 10 --algo ape-gamma --seed $i &
 done
-python3 -m multi_armed_bandit.run_grid_search --noise pareto --moment 1.2 --scale 1.0 --mean one_hot --gap 0.5 --samples 5000 --arms 10 --algo ape-gamma --seed 10
+python3 -m multi_armed_bandit.run_grid_search --noise pareto --moment 1.2 --scale 1.0 --mean one_hot --gap 0.5 --samples 20000 --arms 10 --algo ape-gamma --seed 10
 
 for i in {1..9}
 do
-    python3 -m multi_armed_bandit.run_grid_search --noise pareto --moment 1.2 --scale 1.0 --mean one_hot --gap 0.5 --samples 5000 --arms 10 --algo ape-GEV --seed $i &
+    python3 -m multi_armed_bandit.run_grid_search --noise pareto --moment 1.2 --scale 1.0 --mean one_hot --gap 0.5 --samples 20000 --arms 10 --algo ape-GEV --seed $i &
 done
-python3 -m multi_armed_bandit.run_grid_search --noise pareto --moment 1.2 --scale 1.0 --mean one_hot --gap 0.5 --samples 5000 --arms 10 --algo ape-GEV --seed 10
+python3 -m multi_armed_bandit.run_grid_search --noise pareto --moment 1.2 --scale 1.0 --mean one_hot --gap 0.5 --samples 20000 --arms 10 --algo ape-GEV --seed 10
 
 for i in {1..9}
 do
-    python3 -m multi_armed_bandit.run_grid_search --noise pareto --moment 1.2 --scale 1.0 --mean one_hot --gap 0.5 --samples 5000 --arms 10 --algo ape-pareto --seed $i &
+    python3 -m multi_armed_bandit.run_grid_search --noise pareto --moment 1.2 --scale 1.0 --mean one_hot --gap 0.5 --samples 20000 --arms 10 --algo ape-pareto --seed $i &
 done
-python3 -m multi_armed_bandit.run_grid_search --noise pareto --moment 1.2 --scale 1.0 --mean one_hot --gap 0.5 --samples 5000 --arms 10 --algo ape-pareto --seed 10
+python3 -m multi_armed_bandit.run_grid_search --noise pareto --moment 1.2 --scale 1.0 --mean one_hot --gap 0.5 --samples 20000 --arms 10 --algo ape-pareto --seed 10
 
 for i in {1..9}
 do
-    python3 -m multi_armed_bandit.run_grid_search --noise pareto --moment 1.2 --scale 1.0 --mean one_hot --gap 0.5 --samples 5000 --arms 10 --algo ape-weibull --seed $i &
+    python3 -m multi_armed_bandit.run_grid_search --noise pareto --moment 1.2 --scale 1.0 --mean one_hot --gap 0.5 --samples 20000 --arms 10 --algo ape-weibull --seed $i &
 done
-python3 -m multi_armed_bandit.run_grid_search --noise pareto --moment 1.2 --scale 1.0 --mean one_hot --gap 0.5 --samples 5000 --arms 10 --algo ape-weibull --seed 10
+python3 -m multi_armed_bandit.run_grid_search --noise pareto --moment 1.2 --scale 1.0 --mean one_hot --gap 0.5 --samples 20000 --arms 10 --algo ape-weibull --seed 10
 
 for i in {1..9}
 do
-    python3 -m multi_armed_bandit.run_grid_search --noise pareto --moment 1.2 --scale 1.0 --mean one_hot --gap 0.5 --samples 5000 --arms 10 --algo ape-frechet --seed $i &
+    python3 -m multi_armed_bandit.run_grid_search --noise pareto --moment 1.2 --scale 1.0 --mean one_hot --gap 0.5 --samples 20000 --arms 10 --algo ape-frechet --seed $i &
 done
-python3 -m multi_armed_bandit.run_grid_search --noise pareto --moment 1.2 --scale 1.0 --mean one_hot --gap 0.5 --samples 5000 --arms 10 --algo ape-frechet --seed 10
+python3 -m multi_armed_bandit.run_grid_search --noise pareto --moment 1.2 --scale 1.0 --mean one_hot --gap 0.5 --samples 20000 --arms 10 --algo ape-frechet --seed 10
 
 for i in {1..9}
 do
-    python3 -m multi_armed_bandit.run_grid_search --noise pareto --moment 1.2 --scale 1.0 --mean one_hot --gap 0.5 --samples 5000 --arms 10 --algo ape-uniform --seed $i &
+    python3 -m multi_armed_bandit.run_grid_search --noise pareto --moment 1.2 --scale 1.0 --mean one_hot --gap 0.5 --samples 20000 --arms 10 --algo ape-uniform --seed $i &
 done
-python3 -m multi_armed_bandit.run_grid_search --noise pareto --moment 1.2 --scale 1.0 --mean one_hot --gap 0.5 --samples 5000 --arms 10 --algo ape-uniform --seed 10
+python3 -m multi_armed_bandit.run_grid_search --noise pareto --moment 1.2 --scale 1.0 --mean one_hot --gap 0.5 --samples 20000 --arms 10 --algo ape-uniform --seed 10
 
 for i in {1..9}
 do
-    python3 -m multi_armed_bandit.run_grid_search --noise pareto --moment 1.2 --scale 1.0 --mean one_hot --gap 0.5 --samples 5000 --arms 10 --algo ape-rademacher --seed $i &
+    python3 -m multi_armed_bandit.run_grid_search --noise pareto --moment 1.2 --scale 1.0 --mean one_hot --gap 0.5 --samples 20000 --arms 10 --algo ape-rademacher --seed $i &
 done
-python3 -m multi_armed_bandit.run_grid_search --noise pareto --moment 1.2 --scale 1.0 --mean one_hot --gap 0.5 --samples 5000 --arms 10 --algo ape-rademacher --seed 10
+python3 -m multi_armed_bandit.run_grid_search --noise pareto --moment 1.2 --scale 1.0 --mean one_hot --gap 0.5 --samples 20000 --arms 10 --algo ape-rademacher --seed 10
 
 for i in {1..9}
 do
-    python3 -m multi_armed_bandit.run_grid_search --noise pareto --moment 1.2 --scale 1.0 --mean one_hot --gap 0.5 --samples 5000 --arms 10 --algo mr-ucb --seed $i &
+    python3 -m multi_armed_bandit.run_grid_search --noise pareto --moment 1.2 --scale 1.0 --mean one_hot --gap 0.5 --samples 20000 --arms 10 --algo mr-ucb --seed $i &
 done
-python3 -m multi_armed_bandit.run_grid_search --noise pareto --moment 1.2 --scale 1.0 --mean one_hot --gap 0.5 --samples 5000 --arms 10 --algo mr-ucb --seed 10
+python3 -m multi_armed_bandit.run_grid_search --noise pareto --moment 1.2 --scale 1.0 --mean one_hot --gap 0.5 --samples 20000 --arms 10 --algo mr-ucb --seed 10
